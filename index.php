1000cf7282900813ed5229654d41d9bfedd42d65
Ahmed Sabbour <sabbour@outlook.com>
2018-07-08 11:34:36 +0400

    Added .htaccess to restrict access to AppGW

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
 		<h3>Request details</h3>
 		<ul>
 			<li>HTTP_HOST: <?= getenv('HTTP_HOST'); ?></li>
-			<li>REMOTE_ADDR: <?= getenv('REMOTE_ADDR'); ?></li>
+			<li>HTTP_X_ORIGINAL_HOST: <?= getenv('HTTP_X_ORIGINAL_HOST'); ?></li>
+			<li>HTTP_WAS_DEFAULT_HOSTNAME: <?= getenv('HTTP_WAS_DEFAULT_HOSTNAME'); ?></li>
+			<li>HTTP_X_CLIENT_IP: <?= getenv('HTTP_X_CLIENT_IP'); ?></li>
 			<li>HTTP_CLIENT_IP: <?= getenv('HTTP_CLIENT_IP'); ?></li>
+			<li>REMOTE_ADDR: <?= getenv('REMOTE_ADDR'); ?></li>
 			<li>HTTP_X_FORWARDED_FOR: <?= getenv('HTTP_X_FORWARDED_FOR'); ?></li>
 			<li>HTTP_X_FORWARDED_PROTO: <?= getenv('HTTP_X_FORWARDED_PROTO'); ?></li>
 			<li>HTTP_X_FORWARDED_PORT: <?= getenv('HTTP_X_FORWARDED_PORT'); ?></li>
